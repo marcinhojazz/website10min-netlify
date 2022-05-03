4db1212c3effe9a245cc82dcce54dfaca2f1bf71
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-05-03 12:44:40 -0300

    upgrade site writer details information boilerplate

--- website10min/src/pages/index.js
+++ website10min/src/pages/index.js
@@ -1,7 +1,37 @@
+import Header from 'next/head'
 const Home = () => {
   return (
-    <div>
-      <img src="https://www.moonstats.com/news/wp-content/uploads/2021/10/Metaverse-gaming.jpeg"></img>
+    <div className='container'>
+      <Header>
+        <title>Clean NextJS Boilerplate</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+      </Header>
+
+      <div className='hero'>
+        <h1 className='title'>This site is a clean NextJs boilerplate.</h1>
+        <img src="https://www.moonstats.com/news/wp-content/uploads/2021/10/Metaverse-gaming.jpeg"></img>
+        <div className='details'>
+          <h2>GitHub Respository:</h2>
+          <a href="https://github.com/marcinhojazz/website10min-netlify">
+            https://github.com/marcinhojazz/website10min-netlify
+          </a>
+          <h3>URL Site:</h3>
+          <a href="https://website10min.netlify.app/">
+            https://website10min.netlify.app/
+          </a>
+          <div>
+            <p>
+              My Site:
+            </p>
+            <a src="https://marciomachado.vercel.app/">
+              https://marciomachado.vercel.app/
+            </a>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
