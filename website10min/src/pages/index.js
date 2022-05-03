f852c5496593d810a1431088ff9ed6b116b4dd2e
Marcio Machado Pontes <marcinhojazz@gmail.com>
2022-05-03 12:16:00 -0300

    first commit on project start blank

---
+++ website10min/src/pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <img src="https://www.moonstats.com/news/wp-content/uploads/2021/10/Metaverse-gaming.jpeg"></img>
+    </div>
+  )
+}
+
+export default Home
